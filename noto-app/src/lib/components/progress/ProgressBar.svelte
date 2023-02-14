4abbd6320ca3702f681721a5618e7999bdc879ca
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-02-14 16:20:13 +0500

    progress bar added

---
+++ noto-app/src/lib/components/progress/ProgressBar.svelte
@@ -0,0 +1,20 @@
+<script>
+// @ts-nocheck
+
+	export let title;
+	export let percentage;
+	$: completeClass = title === 'Completed' ? 'bg-green-500' : 'bg-yellow-500';
+</script>
+
+<div>
+	<div class="flex justify-between text-base font-normal text-gray-three">
+		<p>{title}</p>
+		<p>{percentage}%</p>
+	</div>
+	<div class="mt-5 h-4 w-full overflow-hidden rounded-full bg-gray-one">
+		<div
+			class="{completeClass} h-4 rounded-full transition-all duration-500 ease-out"
+			style="width: {percentage}%"
+		/>
+	</div>
+</div>
---
+++ noto-app/src/lib/components/todos/TodoSection.svelte
@@ -0,0 +1,10 @@
+<script>
+	import TodoInput from './TodoInput.svelte';
+	import { notos } from '../../store/NotoStore';
+</script>
+
+<section class="mt-8 space-y-4">
+	{#each $notos as todo (todo.id)}
+		<TodoInput {todo} />
+	{/each}
+</section>
