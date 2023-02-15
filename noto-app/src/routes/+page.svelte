41ffb9d09d1fb39da230d1762a8a6fc4afbfeb4a
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-02-15 15:13:55 +0500

    notes and update form edited

--- noto-app/src/routes/+page.svelte
+++ noto-app/src/routes/+page.svelte
@@ -11,8 +11,8 @@
     
 </script>
 
-<div class="min-h-screen bg-slate-400 selection:bg-green-300 md:p-24 md:px-8">
-    <main class="mx-auto min-h-screen w-full rounded-none bg-slate-200 px-5 py-10 outline-none md:min-h-max max-h-screen md:w-10/12 md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-slate-200">
+<div class="bg-slate-400 selection:bg-green-300 md:p-24 md:px-8">
+    <main class="mx-auto min-h-screen w-full rounded-none bg-slate-200 px-5 py-10 outline-none md:min-h-screen max-h-screen md:w-10/12 md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-slate-200">
 
         <div class="flex items-center justify-evenly pt-5 pb-10">
             <span class="flex-start font-bold text-gray-900 text-2xl">Notes</span>
@@ -34,12 +34,12 @@
 
         {:else}
 
-            <div class="flex space-x-4">
+            <div class="flex space-x-4 max-h-screen">
 
                 <div class="w-1/2 space-y-6">
                     <h2 class="text-2xl font-bold">NOTES</h2>
                     
-                    <div class="flex flex-col space-y-5 h-[23.5%] p-2 overflow-y-scroll">
+                    <div class="flex flex-col space-y-5 h-[75%] p-2 overflow-y-scroll">
                         {#each articles as article}
                             <article class="py-3 px-2 bg-slate-300 outline rounded">
                                 <header class="uppercase font-bold bg-slate-400 -mx-2 px-3 py-2 -mt-3">{article.title}</header>
@@ -55,10 +55,10 @@
                             </article>
                         {/each}
                     </div>
-
+<!-- 
                     <div class="outline rounded-md px-2 -mt-10 pb-2">
                         <ProgressSection />
-                    </div>
+                    </div> -->
                 </div>
 
                 <form class="w-1/2 flex flex-col space-y-2 items-center" action="?/createArticle" method="POST">
--- noto-app/src/routes/[articleId]/+page.svelte
+++ noto-app/src/routes/[articleId]/+page.svelte
@@ -18,11 +18,14 @@
 
 </script>
 
-<form action="?/updateArticle" on:submit={handleSubmit} method="POST">
-	<h3>Editing: {article.title}</h3>
-	<label for="title"> Title </label>
-	<input type="text" id="title" name="title" value={article.title} />
-	<label for="title"> Title </label>
-	<textarea id="content" name="content" rows={5} value={article.content} />
-	<button type="submit">Update Article</button>
-</form>
+<div class="flex flex-col items-center align-middle max-h-[80%] m-12">
+    <form class="w-1/2 flex flex-col space-y-2 items-center" action="?/updateArticle" on:submit={handleSubmit} method="POST">
+        <h3 class="text-2xl font-bold"> Editing: {article.title} </h3>
+        <label class="w-full" for="title"> Title </label>
+        <input class="w-full" type="text" id="title" name="title" value={article.title} />
+        <label class="w-full" for="title"> Note </label>
+        <textarea class="w-full" id="content" name="content" rows={15} value={article.content} />
+        <label class="w-full text-slate-400" for="">!</label>
+        <button class="bg-cyan-500 text-white font-bold hover:bg-cyan-700 text-lg w-1/2 py-2" type="submit"> Update Article </button>
+    </form>
+</div>
