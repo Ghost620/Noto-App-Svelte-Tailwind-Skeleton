4b06c381ef3bbc7d871bb0ee9332ac0da81a57e4
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-02-15 23:00:49 +0500

    layout + db updated

--- noto-app/src/routes/+page.svelte
+++ noto-app/src/routes/+page.svelte
@@ -53,26 +53,27 @@
                         <textarea class="w-full" id="content" name="content" placeholder="Leave a NoTo..." rows={10} />
     
                         <ul class="items-center w-full text-sm font-medium bg-white border border-gray-200 rounded-lg sm:flex dark:bg-slate-500 dark:border-gray-600 text-white">
-                            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-                                <div class="flex items-center pl-5">
+                            
+                            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 flex flex-col items-center">
+                                <div class="flex items-center">
                                     <input id="horizontal-list-radio-license" type="radio" value="" name="list-radio" class="w-4 h-4 text-red-600 bg-gray-100 border-gray-300 focus:ring-red-500">
                                     <label for="horizontal-list-radio-license" class="w-full py-3 ml-2 text-sm font-medium text-white">Not Started </label>
                                 </div>
                             </li>
-                            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-                                <div class="flex items-center pl-5">
+                            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 flex flex-col items-center">
+                                <div class="flex items-center">
                                     <input id="horizontal-list-radio-license" type="radio" value="" name="list-radio" class="w-4 h-4 text-yellow-600 bg-gray-100 border-gray-300 focus:ring-yellow-500">
                                     <label for="horizontal-list-radio-millitary" class="w-full py-3 ml-2 text-sm font-medium text-white">In Progress</label>
                                 </div>
                             </li>
-                            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-                                <div class="flex items-center pl-5">
+                            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 flex flex-col items-center">
+                                <div class="flex items-center">
                                     <input id="horizontal-list-radio-license" type="radio" value="" name="list-radio" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 focus:ring-green-500">
                                     <label for="horizontal-list-radio-passport" class="w-full py-3 ml-2 text-sm font-medium text-white">Complete</label>
                                 </div>
                             </li>
-                            <li class="w-full">
-                                <div class="flex items-center pl-5 ml-6">
+                            <li class="w-full flex flex-col items-center">
+                                <div class="flex items-center">
                                     <button class="relative flex z-10 p-2 focus:outline-none focus:border-blue-400">
                                         <span class="flex items-center pr-4 mt-1 text-white">Share</span>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5 w-6 my-1 text-blue-700">
@@ -82,12 +83,40 @@
                                     </button>
                                 </div>
                             </li>
+                            
                         </ul>
     
                         <button class="bg-cyan-500 text-white font-bold hover:bg-cyan-700 text-lg w-1/2 py-2" type="submit">POST</button>
                     {:else}
                         <textarea class="w-full" id="content" name="content" placeholder="Leave a NoTo..." rows={10} />
-                        <label class="w-full text-slate-200" for="">e</label>
+                        
+                        <ul class="items-center w-full text-sm font-medium bg-white border border-gray-200 rounded-lg sm:flex dark:bg-slate-500 dark:border-gray-600 text-white">
+                            
+                            <li class="w-full flex flex-col items-center">
+                                <div class="flex items-center">
+                                    <button class="relative flex z-10 p-2 focus:outline-none focus:border-blue-400">
+                                        <span class="flex items-center pr-4 mt-1 text-white"> Thread </span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5 w-6 my-1 text-blue-700">
+                                        <path fill="currentColor" d="M352 320c-22.608 0-43.387 7.819-59.79 20.895l-102.486-64.054a96.551 96.551 0 0 0 0-41.683l102.486-64.054C308.613 184.181 329.392 192 352 192c53.019 0 96-42.981 96-96S405.019 0 352 0s-96 42.981-96 96c0 7.158.79 14.13 2.276 20.841L155.79 180.895C139.387 167.819 118.608 160 96 160c-53.019 0-96 42.981-96 96s42.981 96 96 96c22.608 0 43.387-7.819 59.79-20.895l102.486 64.054A96.301 96.301 0 0 0 256 416c0 53.019 42.981 96 96 96s96-42.981 96-96-42.981-96-96-96z">
+                                        </path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </li>
+                            <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 flex flex-col items-center">
+                                <div class="flex items-center">
+                                    <button class="relative flex z-10 p-2 focus:outline-none focus:border-blue-400">
+                                        <span class="flex items-center pr-4 mt-1 text-white">Share</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5 w-6 my-1 text-blue-700">
+                                        <path fill="currentColor" d="M352 320c-22.608 0-43.387 7.819-59.79 20.895l-102.486-64.054a96.551 96.551 0 0 0 0-41.683l102.486-64.054C308.613 184.181 329.392 192 352 192c53.019 0 96-42.981 96-96S405.019 0 352 0s-96 42.981-96 96c0 7.158.79 14.13 2.276 20.841L155.79 180.895C139.387 167.819 118.608 160 96 160c-53.019 0-96 42.981-96 96s42.981 96 96 96c22.608 0 43.387-7.819 59.79-20.895l102.486 64.054A96.301 96.301 0 0 0 256 416c0 53.019 42.981 96 96 96s96-42.981 96-96-42.981-96-96-96z">
+                                        </path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </li>
+
+                        </ul>
+
                         <button class="bg-cyan-500 text-white font-bold hover:bg-cyan-700 text-lg w-1/2 py-2" type="submit">POST</button>
                     {/if}
     
