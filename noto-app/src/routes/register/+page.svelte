1f4e0c5e50db7b1d187394bbe598aae62af3c895
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-02-17 03:53:39 +0500

    register page updated

--- noto-app/src/routes/register/+page.svelte
+++ noto-app/src/routes/register/+page.svelte
@@ -1,18 +1,24 @@
-<form method="POST">
-	<hgroup>
-		<h2>Register</h2>
-		<h3>To post articles, you'll need an account.</h3>
-	</hgroup>
 
-	<label for="name">Name</label>
-	<input type="text" id="name" name="name" required />
 
-	<label for="username">Username</label>
-	<input type="text" id="username" name="username" required />
-
-	<label for="password">Password</label>
-	<input type="password" id="password" name="password" required />
-
-	<button type="submit">Register</button>
-</form>
-<p>Already have an account? <a href="/login">Login</a></p>
+<div class="flex flex-col mt-40 p-6 max-w-7xl m-72">
+	<form method="POST" class="space-y-6 flex flex-col">
+		<hgroup class="space-y-2">
+			<h2 class="text-gray-800 text-4xl font-bold">Register</h2>
+			<h3 class="font-medium">To post Notos, you'll need an account.</h3>
+		</hgroup>
+	
+		<div class="flex flex-col">
+			<label class="font-medium my-2" for="name">Name</label>
+			<input class="rounded-lg" type="text" id="name" name="name" required />
+		
+			<label class="font-medium my-2" for="username">Username</label>
+			<input class="rounded-lg" type="text" id="username" name="username" required />
+		
+			<label class="font-medium my-2" for="password">Password</label>
+			<input class="rounded-lg" type="password" id="password" name="password" required />
+		</div>
+	
+		<button type="submit" class="rounded-lg font-semibold h-12 text-white bg-green-700 hover:bg-green-800">Register</button>
+	</form>
+	<p class="mt-4">Already have an account? <a href="/login" class="underline hover:text-white">Login</a></p>
+</div>
