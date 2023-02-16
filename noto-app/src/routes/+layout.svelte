9d7eb8db730c62d54ab8adaf7bb2d3747ce57c4c
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-02-17 01:03:57 +0500

    session update

--- noto-app/src/routes/+layout.svelte
+++ noto-app/src/routes/+layout.svelte
@@ -1,23 +1,18 @@
-<script    async script lang="ts">
+<script lang="ts">
     import "../app.css";
-	import { notos } from '../lib/store/NotoStore';
-	const func = async () => {
-		const response = await fetch('/', { method: 'POST' });
-		const data = await response.json();
-		console.log(data);
-	}
+	import { notos } from '../lib/store/NotoStore'
 </script>
 
-<header class="bg-blue-500 text-white flex justify-between items-center py-2 px-6">
-	<ul class="w-full">
+<header class="bg-slate-800 z-20 text-white flex justify-between items-center py-2 px-6 top-0 fixed w-full">
+	<ul class="w-full py-1">
 		<form method="POST" class="flex justify-between w-full px-24">
-			<li><a href="/">Home</a></li>
-			{#if true}
-				<li><a href="/register">Register</a></li>
-				<li><a href="/login" role="button">Login</a></li>
-			<!-- {:else} -->
-				<li>
-					<button formaction="/logout" type="submit">Logout</button>
+			<li class="bg-slate-400 text-gray-800 font-bold px-6 py-2 rounded-lg hover:text-white"><a href="/">Home</a></li>
+			{#if !$notos}
+				<li class="bg-slate-400 text-gray-800 font-bold px-6 py-2 rounded-lg hover:text-white"><a href="/register">Register</a></li>
+				<li class="bg-slate-400 text-gray-800 font-bold px-6 py-2 rounded-lg hover:text-white"><a href="/login" role="button">Login</a></li>
+			{:else}
+				<li class="bg-slate-400 text-gray-800 font-bold px-6 py-2 rounded-lg hover:text-white">
+					<button formaction="/logout" type="submit" on:click={() => window.localStorage.setItem('user', 'false')}>Logout</button>
 				</li>
 			{/if}
 		</form>
