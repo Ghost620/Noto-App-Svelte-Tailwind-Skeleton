4c9a7f2ee0c5e5b49d0217477db8523a27680e7d
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-02-16 09:16:14 +0500

    update page edited

--- noto-app/src/routes/[articleId]/+page.svelte
+++ noto-app/src/routes/[articleId]/+page.svelte
@@ -19,13 +19,9 @@
 </script>
 
 <div class="flex flex-col items-center align-middle max-h-[80%] m-12">
-    <form class="w-1/2 flex flex-col space-y-2 items-center" action="?/updateArticle" on:submit={handleSubmit} method="POST">
-        <h3 class="text-2xl font-bold"> Editing: {article.title} </h3>
-        <label class="w-full" for="title"> Title </label>
-        <input class="w-full" type="text" id="title" name="title" value={article.title} />
-        <label class="w-full" for="title"> Note </label>
+    <form class="w-1/2 flex flex-col space-y-12 items-center" action="?/updateArticle" on:submit={handleSubmit} method="POST">
+        <h3 class="text-2xl font-bold"> Editing: {article.type.split('-')[0]} </h3>
         <textarea class="w-full" id="content" name="content" rows={15} value={article.content} />
-        <label class="w-full text-slate-400" for="">!</label>
-        <button class="bg-cyan-500 text-white font-bold hover:bg-cyan-700 text-lg w-1/2 py-2" type="submit"> Update Article </button>
+        <button class="bg-cyan-500 text-white font-bold hover:bg-cyan-700 text-lg w-1/2 py-2" type="submit"> Update Noto </button>
     </form>
 </div>
